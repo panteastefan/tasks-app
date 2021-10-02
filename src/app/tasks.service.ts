add2c563b096bcef744cbe6effb7cc8067c575e2
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-02 14:57:38 +0300

    Added getTasks into tasks.service

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  private apiServerUrl = '';
+  constructor(private http: HttpClient) { }
+
+  public getTasks(): Observable<any> {
+    return this.http.get<any>(`${this.apiServerUrl}/tasks/all`);
+  }
 }
