29baf402d73110ce4cc8ffe561c3ab5f75f619bd
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-02 16:22:10 +0300

    Added Task and TasksServices methods

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -10,7 +10,19 @@ export class TasksService {
   private apiServerUrl = '';
   constructor(private http: HttpClient) { }
 
-  public getTasks(): Observable<any> {
-    return this.http.get<any>(`${this.apiServerUrl}/tasks/all`);
+  public getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiServerUrl}/tasks/all`);
+  }
+
+  public addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${this.apiServerUrl}/tasks/add`, task);
+  }
+
+  public updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiServerUrl}/tasks/add`, task);
+  }
+
+  public deleteTask(taskId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/tasks/delete/${taskId}`);
   }
 }
