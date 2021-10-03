4a5eb113ac16ac3998d7600dc7c12258668a49a6
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-03 18:21:38 +0300

    Deleted unnecesary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { UserTasksTableComponent } from './components/user-tasks-table/user-tasks-table.component';
-import { TaskElementComponent } from './components/task-element/task-element.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {TabsModule} from "ngx-bootstrap/tabs";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     UserTasksTableComponent,
-    TaskElementComponent
   ],
   imports: [
     BrowserModule,
