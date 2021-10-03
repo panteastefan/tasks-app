cdc2106b6504ce9296549fbd033a4490b27a6948
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-03 22:48:55 +0300

    Added login service

---
+++ src/app/services/users/login.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private apiServerUrl = environment.apiBaseUrl;
+  constructor(private http: HttpClient) { }
+
+  public login(username: String, password: String): Observable<any> {
+    return this.http.post<any>(`${this.apiServerUrl}/login/add`, {username, password});
+  }
+}
