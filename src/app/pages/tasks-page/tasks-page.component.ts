9a4c8ab340430af67878f8dca68c2a904affb738
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-03 21:00:10 +0300

    Added tasksPage

---
+++ src/app/pages/tasks-page/tasks-page.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Task} from "../../services/tasks/task";
+import {TasksService} from "../../services/tasks/tasks.service";
+import {HttpErrorResponse} from "@angular/common/http";
+
+@Component({
+  selector: 'app-tasks-page',
+  templateUrl: './tasks-page.component.html',
+  styleUrls: ['./tasks-page.component.css']
+})
+export class TasksPageComponent implements OnInit {
+
+  public tasks!: Task[];
+  constructor(private taskService: TasksService) { }
+
+  ngOnInit(): void {
+    this.getUserTasks();
+  }
+  public getUserTasks(): void {
+    this.taskService.getUserTasks().subscribe(
+      (response: Task[]) => {
+        this.tasks = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+}
