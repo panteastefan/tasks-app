f3a9c24a7322515ddb91c40eed2d17e028d3fb3b
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-03 16:05:54 +0300

    Added TasksTable

--- src/app/components/user-tasks-table/user-tasks-table.component.ts
+++ src/app/components/user-tasks-table/user-tasks-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-tasks-table',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserTasksTableComponent implements OnInit {
 
-  constructor() { }
+  @Input() tasks: any;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
