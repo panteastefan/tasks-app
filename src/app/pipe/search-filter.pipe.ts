5816e520dee188568d45f799128800438dc8942c
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-10 14:32:27 +0300

    Added all filters except dueDate

--- src/app/pipe/search-filter.pipe.ts
+++ src/app/pipe/search-filter.pipe.ts
@@ -5,6 +5,11 @@ import {Task} from "../models/task";
 
 @Pipe({ name: 'appFilter' })
 export class SearchFilterPipe implements PipeTransform {
+  datesAreOnSameDay = (first: Date, second: Date) =>
+    first.getFullYear() === second.getFullYear() &&
+    first.getMonth() === second.getMonth() &&
+    first.getDate() === second.getDate();
+
   transform(tasks: Task[], taskFilter: Task): any[] {
     if (!tasks) {
       return [];
@@ -13,9 +18,21 @@ export class SearchFilterPipe implements PipeTransform {
       return tasks;
     }
     const name = taskFilter.name.toLocaleLowerCase();
+    const description = taskFilter.description.toLocaleLowerCase();
+    const dueDate = taskFilter.dueDate;
+    console.log("due date: ", dueDate)
+    const username = taskFilter.username.toLocaleLowerCase();
+    const status = taskFilter.status;
+
 
     return tasks.filter(task => {
-      return task.name.toLocaleLowerCase().includes(name);
+      console.log(task.dueDate)
+      return task.name.toLocaleLowerCase().includes(name) &&
+            task.description.toLocaleLowerCase().includes(description) &&
+            task.username.toLocaleLowerCase().includes(username) &&
+            // this.datesAreOnSameDay(task.dueDate, dueDate) &&
+            // task.dueDate.getDate() dueDate &&
+            task.status == status;
     });
   }
 }
