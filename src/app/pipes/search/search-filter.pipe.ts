f0c884fda838a5affba29e644af178b5577a65db
Pantea Stefan <panteastefan97@yahoo.com>
2021-10-11 19:03:10 +0300

    Added date filter in filter pipe

--- src/app/pipes/search/search-filter.pipe.ts
+++ src/app/pipes/search/search-filter.pipe.ts
@@ -1,15 +1,8 @@
-// filter.pipe.ts
-
 import { Pipe, PipeTransform } from '@angular/core';
 import {Task} from "../../models/task";
 
 @Pipe({ name: 'appFilter' })
 export class SearchFilterPipe implements PipeTransform {
-  datesAreOnSameDay = (first: Date, second: Date) =>
-    first.getFullYear() === second.getFullYear() &&
-    first.getMonth() === second.getMonth() &&
-    first.getDate() === second.getDate();
-
   transform(tasks: Task[], taskFilter: Task): any[] {
     if (!tasks) {
       return [];
@@ -19,19 +12,16 @@ export class SearchFilterPipe implements PipeTransform {
     }
     const name = taskFilter.name.toLocaleLowerCase();
     const description = taskFilter.description.toLocaleLowerCase();
-    // const dueDate = taskFilter.dueDate;
-    // console.log("due date: ", dueDate)
+    const dueDate = new Date(taskFilter.dueDate);
     const username = taskFilter.username.toLocaleLowerCase();
     const status = taskFilter.status;
 
 
     return tasks.filter(task => {
-      // console.log(task.dueDate)
       return task.name.toLocaleLowerCase().includes(name) &&
             task.description.toLocaleLowerCase().includes(description) &&
             task.username.toLocaleLowerCase().includes(username) &&
-            // this.datesAreOnSameDay(task.dueDate, dueDate) &&
-            // task.dueDate.getDate() dueDate &&
+            new Date(task.dueDate) > dueDate &&
             task.status == status;
     });
   }
